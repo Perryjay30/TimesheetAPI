8ca93f0d1c2e111c8c068ff3c0f52af022be9a08
Perryjay30 <pelumijsh@gmail.com>
2023-06-05 23:27:24 +0100

    TimesheetAPI Controller

--- src/main/java/com/codingchallenge/timesheetapi/data/dto/RegisterUserRequest.java
+++ src/main/java/com/codingchallenge/timesheetapi/data/dto/RegisterUserRequest.java
@@ -1,9 +1,6 @@
 package com.codingchallenge.timesheetapi.data.dto;
 
-import com.codingchallenge.timesheetapi.data.model.Role;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.mindrot.jbcrypt.BCrypt;
 
--- src/main/java/com/codingchallenge/timesheetapi/data/model/Role.java
+++
@@ -1,5 +0,0 @@
-package com.codingchallenge.timesheetapi.data.model;
-
-public enum Role {
-    USER, ADMIN
-}
